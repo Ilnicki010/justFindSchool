dcb8461aeef5226ce59798a1fb87652dcfffa924
Dominik Ilnicki <dominik.ilnicki@gmail.com>
2019-08-04 10:10:33 +0200

    init

--- src/router.js
+++ src/router.js
@@ -3,23 +3,18 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 
 Vue.use(Router);
-
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/join',
+      name: 'join',
+      component: () => import('./views/Join.vue'),
     },
   ],
 });
---
+++ src/views/Join.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Join Aske, ask your friends about everything.</h1>
+  </div>
+</template>
